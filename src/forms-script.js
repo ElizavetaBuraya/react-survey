8ea1fed4a1283565d3ffc8cfbfde2a542d588670
Unknown <lizaburaya@gmail.com>
2017-07-13 17:16:07 +0300

    Added toggle scripts

--- src/forms-script.js
+++ src/forms-script.js
@@ -5,7 +5,27 @@ $("#file").change(function(event) {
         $(".filepath").html(fileName);
 });
 
-$("#question-popup").click(function() {
-    $("#question-content").toggle();
+$(".toggle-question-params").click(function() {
+    let quest_visibility =  $(".question-params-toggle-panel").css("visibility");
+    let survey_visibility =  $(".survey-params-toggle-panel").css("visibility");
+    if (quest_visibility == "hidden") {
+        $(".question-params-toggle-panel").css("visibility", "visible");
+        $(".survey-params-toggle-panel").css("visibility", "hidden");
+    }
+    else {
+        $(".question-params-toggle-panel").css("visibility", "hidden");
+    }
+})
+
+$(".toggle-survey-params").click(function() {
+    let survey_visibility =  $(".survey-params-toggle-panel").css("visibility");
+    let quest_visibility =  $(".question-params-toggle-panel").css("visibility");
+    if (survey_visibility == "hidden") {
+        $(".survey-params-toggle-panel").css("visibility", "visible");
+        $(".question-params-toggle-panel").css("visibility", "hidden");
+    }
+    else {
+        $(".survey-params-toggle-panel").css("visibility", "hidden");
+    }
 })
 
