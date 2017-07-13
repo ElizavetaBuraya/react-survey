d3864e58ea2826061a54a313cfb0e8da9d012fce
Unknown <lizaburaya@gmail.com>
2017-07-13 17:16:31 +0300

    Imported scripts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import 'font-awesome/scss/font-awesome.scss';
 import style from '../sass/style.scss';
 import style from '../less/style.less';
+import './edit-template.js';
+import './forms-script.js';
+
