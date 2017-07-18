d97675c2397affe5bd443f9e8547ec5ce78c4a12
Unknown <lizaburaya@gmail.com>
2017-07-18 14:24:21 +0300

    Imported Login_Register component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import './components/Login_Register.jsx';
 import './libraries/Chart.min.js';
 import './custom_scripts/edit-template.js';
 import './libraries/rating.min.js';
 import './custom_scripts/forms-script.js';
+
 
