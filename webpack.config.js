b7bcfdb9378c68153ae17e7af12454b71c79f679
Unknown <lizaburaya@gmail.com>
2017-07-18 14:23:48 +0300

    Added react settings

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env']
+                        presets:[
+                            'env', 'react'
+                        ]
                     }
                 }
             },
