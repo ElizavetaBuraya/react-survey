d3e21edc0d2b3de045faa60c25d44f17f3c0f834
Unknown <lizaburaya@gmail.com>
2017-07-21 19:22:08 +0300

    Deleted link hrefs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,10 +5,10 @@ export default class Sidebar extends React.Component {
     render() {
         return(
             <div className="d-flex flex-column sidebar hidden-sm-down">
-                <Link to='/new_survey' className="sidebar-nav" href="new_survey.html">Новый опрос</Link>
-                <Link to='/surveys' className="sidebar-nav" href="surveys.html">Мои опросы</Link>
-                <Link to='/templates' className="sidebar-nav" href="templates.html">Шаблоны опросов</Link>
-                <Link to='/users' className="sidebar-nav" href="users.html">Пользователи</Link>
+                <Link to='/new_survey' className="sidebar-nav">Новый опрос</Link>
+                <Link to='/surveys' className="sidebar-nav">Мои опросы</Link>
+                <Link to='/templates' className="sidebar-nav">Шаблоны опросов</Link>
+                <Link to='/users' className="sidebar-nav">Пользователи</Link>
             </div>
         )
     }
