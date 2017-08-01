b513c58db60cd427e9cd9b9f3e0ea60e11faab13
Unknown <lizaburaya@gmail.com>
2017-08-01 18:37:11 +0300

    Added currentPage check for active-nav

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,10 +5,26 @@ export default class Sidebar extends React.Component {
     render() {
         return(
             <div className="d-flex flex-column sidebar hidden-sm-down">
-                <Link to='/new_survey' className="sidebar-nav">Новый опрос</Link>
-                <Link to='/surveys' className="sidebar-nav">Мои опросы</Link>
-                <Link to='/templates' className="sidebar-nav">Шаблоны опросов</Link>
-                <Link to='/users' className="sidebar-nav">Пользователи</Link>
+                <Link to='/new_survey'
+                      className={this.props.currentPage === '/new_survey'
+                          ? "sidebar-nav active-nav"
+                          : "sidebar-nav"}>Новый опрос
+                </Link>
+                <Link to='/surveys'
+                      className={this.props.currentPage === '/surveys'
+                          ? "sidebar-nav active-nav"
+                          : "sidebar-nav"}>Мои опросы
+                </Link>
+                <Link to='/templates'
+                      className={this.props.currentPage === '/templates'
+                          ? "sidebar-nav active-nav"
+                          : "sidebar-nav"}>Шаблоны опросов
+                </Link>
+                <Link to='/users'
+                      className={this.props.currentPage === '/users'
+                          ? "sidebar-nav active-nav"
+                          : "sidebar-nav"}>Пользователи
+                </Link>
             </div>
         )
     }
