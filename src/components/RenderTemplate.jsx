3e23a3d7c8ffb6e11bd19603d0c6fcbcfc9cd254
Unknown <lizaburaya@gmail.com>
2017-08-01 18:36:45 +0300

    Fixed questions and pages render

--- src/components/RenderTemplate.jsx
+++ src/components/RenderTemplate.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Template extends React.Component {
+export default class RenderTemplate extends React.Component {
     render() {
         let that = this;
         let templates = this.props.data.map(function(obj, index) {
@@ -12,13 +12,18 @@ export default class Template extends React.Component {
                     </span>
                 <h2>{ obj.name }</h2>
                 <p>{ obj.description }</p>
-                <p className="question-number">Вопросов: <span>{ obj.pages }&nbsp;</span></p>
-                <p className="page-number">Страниц: <span>{ obj.questions }</span></p>
+                <p className="question-number">Вопросов: <span>{ obj.questions }&nbsp;</span></p>
+                <p className="page-number">Страниц: <span>{ obj.pages }</span></p>
                 <Link to="/new_survey" className="create-survey">Создать опрос</Link>
             </div>
         });
         let filteredTemplates = this.props.data.map(function(obj, index) {
-            if ((that.props.filterText) && obj.name.includes(that.props.filterText)) {
+            if ((that.props.filterText) &&
+                obj.name.includes(that.props.filterText) ||
+                obj.description.includes(that.props.filterText) ||
+                obj.pages.toString().includes(that.props.filterText) ||
+                obj.questions.toString().includes(that.props.filterText)
+             ) {
                 return <div className="template" key={index}>
                     <span className="active-template">
                         <Link to={`${obj.link}`} className="edit-template"/>
@@ -26,8 +31,8 @@ export default class Template extends React.Component {
                     </span>
                     <h2>{ obj.name }</h2>
                     <p>{ obj.description }</p>
-                    <p className="question-number">Вопросов: <span>{ obj.pages }&nbsp;</span></p>
-                    <p className="page-number">Страниц: <span>{ obj.questions }</span></p>
+                    <p className="question-number">Вопросов: <span>{ obj.questions }&nbsp;</span></p>
+                    <p className="page-number">Страниц: <span>{ obj.pages }</span></p>
                     <Link to="/new_survey" className="create-survey">Создать опрос</Link>
                 </div>
             }
