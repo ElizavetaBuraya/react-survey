cea6ca457d4b3c4386df0da68ae24a82d4109729
Unknown <lizaburaya@gmail.com>
2017-08-03 20:18:37 +0300

    Added edit-question.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import './libraries/Chart.min.js';
+import './custom_scripts/edit-question.js';
 import './custom_scripts/edit-template.js';
 import './libraries/rating.min.js';
 import './custom_scripts/forms-script.js';
