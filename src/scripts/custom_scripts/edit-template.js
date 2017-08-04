aa06e881aa33ee513abb5f09f10c50c47436656e
Unknown <lizaburaya@gmail.com>
2017-08-04 13:10:21 +0300

    Moved scripts to scripts/ folder

--- src/scripts/custom_scripts/edit-template.js
+++ src/scripts/custom_scripts/edit-template.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(document).on('click', '.template', (function(event) {
-        event.stopPropagation();
-        $(".template").css("box-shadow", "none");
-        $(this).find(".active-template").show();
-        $(this).css("box-shadow","inset 0em 0em 0em .4em #e6e6e6");
-    })
-)});
+            event.stopPropagation();
+            $(".template").css("box-shadow", "none");
+            $(this).parent(".template-wrapper").find(".active-template").show();
+            $(this).css("box-shadow","inset 0em 0em 0em .4em #e6e6e6");
+        })
+    )});
 
 function hideTemplate() {
     $(".template").css("box-shadow", "none");
