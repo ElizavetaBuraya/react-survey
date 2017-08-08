18a8a2701c7d50dfb11e54e1ff9c491aae8b25e9
Unknown <lizaburaya@gmail.com>
2017-08-08 10:23:14 +0300

    Moved AboutContent and SurveyContent to a single component

--- src/scripts/components/Tabs.jsx
+++ src/scripts/components/Tabs.jsx
@@ -4,7 +4,11 @@ import GenerateQuestions from './GenerateQuestions.jsx'
 
 function TabItem(props) {
     return  <li className='nav-item nav-tab-item'>
-                <a className={`nav-link ${props.active}`} data-toggle='tab' href={props.href} role='tab'>{props.name}</a>
+                <a className={(props.active ? 'active ' : '') + 'nav-link'}
+                   data-toggle='tab'
+                   href={props.href}
+                   role='tab'>{props.name}
+                </a>
             </li>
 }
 
@@ -25,53 +29,45 @@ class TabsList extends React.Component {
     }
 }
 
-class AboutContent extends React.Component {
+class GenerateContent extends React.Component {
     render() {
-        const tabClass = classNames({'tab-pane about-pane': true,
-            'active': this.props.isRegistered
+            const tabClass = classNames({'tab-pane': true,
+                'about-pane': this.props.currentPage === '/about',
         });
         const tabs = this.props.navtabs;
+        const isAboutPage = (this.props.currentPage === '/about');
+        const isSurveyPage = (this.props.currentPage === '/new_survey');
         return (
             <div className='tab-content'>
                 {tabs.map((tab, index) =>
-                    <div className={tabClass} id={tab.id} role='tabpanel' key={index}>
-                        <img src='img/placeholder_2.png' alt='placeholder' height='200' />
-                        <p>
-                            Page {index + 1}.<br />
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
-                            elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
-                            Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
-                            fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
-                            eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
-                            malesuada fames ac ante ipsum primis in faucibus.
-                        </p>
-                    </div>
-                )}
-            </div>
-        )
-    }
-}
-
-class SurveyContent extends React.Component {
-    render() {
-        const tabClass = classNames({'tab-pane active': true
-        });
-        const tabs = this.props.navtabs;
-        return (
-            <div className='tab-content'>
-                {tabs.map((tab, index) =>
-                    <div className={tabClass} id={tab.id} role='tabpanel' key={index}>
-                        <div className='survey-content'>
-                            <i className='fa fa-trash fa-lg' aria-hidden='true' />
-                            <input type='text' name='page-head' id='page_name' placeholder='Страница 1' /><br/>
-                            {this.props.questions &&
-                                <GenerateQuestions questions = {this.props.questions}
-                                                   handleUpdateQuestion = {this.props.handleUpdateQuestion}
-                                                   handleDragQuestion = {this.props.handleDragQuestion}
-                                                   handleDeleteQuestion = {this.props.handleDeleteQuestion}
-                                />
-                            }
-                        </div>
+                    <div className={(tab.active ? 'active ' : '') + tabClass} id={tab.id} role='tabpanel' key={index}>
+                        {isSurveyPage &&
+                            <div className='survey-content'>
+                                <i className='fa fa-trash fa-lg' aria-hidden='true' />
+                                <input type='text' name='page-head' id='page_name' placeholder='Страница 1' /><br/>
+                                {this.props.questions &&
+                                    <GenerateQuestions questions = {this.props.questions}
+                                                       handleUpdateQuestion = {this.props.handleUpdateQuestion}
+                                                       handleDragQuestion = {this.props.handleDragQuestion}
+                                                       handleDeleteQuestion = {this.props.handleDeleteQuestion}
+                                    />
+                                }
+                            </div>
+                        }
+                        {isAboutPage &&
+                            <div>
+                                <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+                                <p>
+                                    Page {index + 1}.<br />
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                                    elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                                    Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                                    fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                                    eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                                    malesuada fames ac ante ipsum primis in faucibus.
+                                </p>
+                            </div>
+                        }
                     </div>
                 )}
             </div>
@@ -81,18 +77,18 @@ class SurveyContent extends React.Component {
 
 export default class Tabs extends React.Component {
     render() {
-        let isAboutPage = (this.props.currentPage === '/about');
         let isSurveyPage = (this.props.currentPage === '/new_survey');
         return(
             <div className={isSurveyPage ? 'survey-page' : ''}>
                 <TabsList navtabs={this.props.navtabs}/>
                 <div className='tab-content'>
-                    {isAboutPage && <AboutContent navtabs={this.props.navtabs}/>}
-                    {isSurveyPage && <SurveyContent navtabs={this.props.navtabs}
-                                                    questions = {this.props.questions}
-                                                    handleUpdateQuestion = {this.props.handleUpdateQuestion}
-                                                    handleDragQuestion = {this.props.handleDragQuestion}
-                                                    handleDeleteQuestion = {this.props.handleDeleteQuestion}/>}
+                    <GenerateContent navtabs={this.props.navtabs}
+                                     currentPage={this.props.currentPage}
+                                     questions = {this.props.questions}
+                                     handleUpdateQuestion = {this.props.handleUpdateQuestion}
+                                     handleDragQuestion = {this.props.handleDragQuestion}
+                                     handleDeleteQuestion = {this.props.handleDeleteQuestion}
+                    />
                 </div>
             </div>
         )
