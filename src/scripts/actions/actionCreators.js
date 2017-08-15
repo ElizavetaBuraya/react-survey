10020af4c431d5fc9a1ed9fe0db3120f6d005b6b
Unknown <lizaburaya@gmail.com>
2017-08-15 18:54:11 +0300

    Created actionCreators

--- src/scripts/actions/actionCreators.js
+++ src/scripts/actions/actionCreators.js
@@ -1,29 +1,8 @@
 import {actionTypes} from './actionTypes';
 
-export function updateHistory(history){
+export function generateQuestions(questions){
     return {
-        type: actionTypes.UPDATE_HISTORY,
-        payload: history
-    };
-}
-
-export function generatePairs(pairs){
-    return {
-        type: actionTypes.GENERATE_PAIRS,
-        payload: pairs
-    };
-}
-
-export function updateTextAreaHeight(height){
-    return {
-        type: actionTypes.UPDATE_TEXT_AREA_HEIGHT,
-        payload: height
-    };
-}
-
-export function toggleCanGenerate(canGenerate){
-    return {
-        type: actionTypes.TOGGLE_CAN_GENERATE,
-        payload: canGenerate
+        type: actionTypes.GENERATE_QUESTIONS,
+        payload: questions
     };
 }
