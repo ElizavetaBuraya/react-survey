bce7963efabd3485f1f4f7fd915c3fa925e4833d
Unknown <lizaburaya@gmail.com>
2017-08-15 18:51:26 +0300

    Added reducer

--- src/scripts/reducers/reducer.js
+++ src/scripts/reducers/reducer.js
@@ -1,22 +1,13 @@
 import {actionTypes} from '../actions/actionTypes';
 
 const initialState = {
-  history: [],
-  pairs: [],
-  canGenerate: true,
-  height: 1
+  questions: null,
 };
 
 export default function renderApp(state = initialState, action) {
     switch(action.type){
-        case actionTypes.UPDATE_HISTORY:
-            return {...state, history: action.payload};
-        case actionTypes.GENERATE_PAIRS:
-            return {...state, pairs: action.payload};
-        case actionTypes.UPDATE_TEXT_AREA_HEIGHT:
-            return {...state, height: action.payload};
-        case actionTypes.TOGGLE_CAN_GENERATE:
-            return {...state, canGenerate: action.payload};
+        case actionTypes.GENERATE_QUESTIONS:
+            return {...state, questions: action.payload};
         default:
             return state
     }
