df3a091ced5dcdcf96a94baf5020536c09ea0bf8
Unknown <lizaburaya@gmail.com>
2017-08-16 10:37:41 +0300

    Removed actionCreator

--- src/scripts/actions/actionCreators.js
+++ src/scripts/actions/actionCreators.js
@@ -2,7 +2,6 @@ import {actionTypes} from './actionTypes';
 
 export function generateQuestions(questions){
     return {
-        type: actionTypes.GENERATE_QUESTIONS,
-        payload: questions
+
     };
 }
