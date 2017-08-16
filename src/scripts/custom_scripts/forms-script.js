fff4c8e786c10b151d5c6c65479c932ceda65249
Unknown <lizaburaya@gmail.com>
2017-08-16 18:08:22 +0300

    Moved filepath loading script to react

--- src/scripts/custom_scripts/forms-script.js
+++ src/scripts/custom_scripts/forms-script.js
@@ -1,12 +1,5 @@
 import Chart from 'chart.js';
 
-$("#file").change(function(event) {
-    let fileName = '';
-    fileName = event.target.value.split('\\').pop();
-    if(fileName)
-        $(".filepath").html(fileName);
-});
-
 document.addEventListener("DOMContentLoaded", function(event) {
     (function () {
         var data_1 = {
