a73792607f0123b82f48ea0fb443d9fbac4d4612
Unknown <lizaburaya@gmail.com>
2017-08-18 17:45:55 +0300

    Created reducers

--- src/scripts/reducers/reducer.js
+++ src/scripts/reducers/reducer.js
@@ -1,11 +1,23 @@
 import {actionTypes} from '../actions/actionTypes';
 
 const initialState = {
-  questions: null,
+    isRegistered: JSON.parse(sessionStorage.getItem('isRegistered')),
+    isAuthorized: JSON.parse(sessionStorage.getItem('isAuthorized')),
+    loggedInAs: JSON.parse(sessionStorage.getItem('loggedInAs')),
+    currentPage: '',
 };
 
 export default function renderApp(state = initialState, action) {
     switch(action.type){
+        case actionTypes.LOG_IN:
+            sessionStorage.setItem('isAuthorized', action.payload);
+            sessionStorage.setItem('loggedInAs', JSON.stringify(action.data));
+            return {...state, isAuthorized: action.payload, loggedInAs: action.data};
+        case actionTypes.TOGGLE_REGISTERED:
+            sessionStorage.setItem('isRegistered', action.payload);
+            return {...state, isRegistered: action.payload};
+        case actionTypes.UPDATE_CURRENT_PAGE:
+            return {...state, currentPage: action.payload};
         default:
             return state
     }
