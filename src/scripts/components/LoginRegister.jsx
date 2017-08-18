de835aa18c0c18c99b6e97cb009dfeab406019a3
Unknown <lizaburaya@gmail.com>
2017-08-18 17:44:09 +0300

    Moved form logic to redux-forms

--- src/scripts/components/LoginRegister.jsx
+++ src/scripts/components/LoginRegister.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import Form from './LoginRegisterForm.jsx'
 
 export default class LoginRegister extends React.Component {
-    constructor () {
-        super();
-    }
-
     componentDidMount() {
-        if(this.props.isAuthorized) {
-            this.props.handleLogOutClick();
-        }
         if (this.props.currentPage !== '/')
         {
             this.props.handleChangePage('/');
@@ -17,42 +11,28 @@ export default class LoginRegister extends React.Component {
     }
 
     render() {
+        const isAuthorized = this.props.isAuthorized;
         const isRegistered = this.props.isRegistered;
+        const userRole = (this.props.loggedInAs) ? this.props.loggedInAs.role : null;
+
+        if (isAuthorized) {
+            if (userRole === 'Администратор') {
+                return (
+                    <Redirect to="/surveys"/>
+                )
+            } else {
+                return (
+                    <Redirect to="/about"/>
+                )
+            }
+        }
+
         return (
             <main className='d-flex flex-row justify-content-center align-items-center'>
-                <form className='login-form'>
-                    <h2 className='login-form-heading'>{isRegistered ? 'Вход' : 'Регистрация'}</h2>
-                    {isRegistered &&
-                    <div>
-                        <label htmlFor='inputLogin' className='sr-only'>Логин</label>
-                        <input type='text' id='inputLogin' className='form-control' placeholder='Логин' required autoFocus />
-                        <label htmlFor='inputPassword' className='sr-only'>Пароль</label>
-                        <input type='password' id='inputPassword' className='form-control' placeholder='******' required />
-                        <div className='d-flex justify-content-between'>
-                            <a className='signup' href='#' onClick={this.props.handleNotRegisteredClick}>Регистрация</a>
-                            <a className='getpassword' href='#'>Забыли пароль?</a>
-                        </div>
-                        <button className='login-form-btn' type='submit'>
-                            <Link className='login-redirect' to='/starter_page' onClick={this.props.handleLogInClick}>Войти</Link>
-                        </button>
-                    </div>
-                    }
-                    {!isRegistered &&
-                    <div>
-                        <label htmlFor='inputName' className='sr-only'>Имя</label>
-                        <input type='text' id='inputName' className='form-control' placeholder='Имя' required autoFocus />
-                        <label htmlFor='inputEmail' className='sr-only'>E-mail</label>
-                        <input type='email' id='inputEmail' className='form-control' placeholder='Логин (e-mail)' required />
-                        <label htmlFor='inputPassword' className='sr-only'>Пароль</label>
-                        <input type='password' id='inputPassword' className='form-control' placeholder='Пароль' required />
-                        <label htmlFor='inputRepeatPassword' className='sr-only'>Повторить пароль</label>
-                        <input type='password' id='inputRepeatPassword' className='form-control' placeholder='Повторить пароль' required />
-                        <button className='login-form-btn' type='submit'>
-                            <Link className='login-redirect' to='/starter_page' onClick={this.props.handleLogInClick}>Создать аккаунт</Link>
-                        </button>
-                    </div>
-                    }
-                </form>
+                <Form onSubmit={(isRegistered) ? this.props.handleLogInClick : this.props.handleCreateUserClick}
+                      isRegistered = {this.props.isRegistered}
+                      handleNotRegisteredClick = {this.props.handleNotRegisteredClick}
+                />
             </main>
         )
     }
