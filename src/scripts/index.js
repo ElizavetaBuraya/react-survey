e2d5470181a7587ff6d66de748f70c014b4d69fc
Unknown <lizaburaya@gmail.com>
2017-08-18 17:45:30 +0300

    Added redux devtools

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 import AppContainer from './containers/AppContainer';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render((
     <Provider store={store}>
