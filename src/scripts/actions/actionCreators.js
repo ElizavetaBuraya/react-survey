439df220f27c056b3cd1a291a0de423ea963e13d
Unknown <lizaburaya@gmail.com>
2017-08-18 17:38:17 +0300

    Added actionCreators

--- src/scripts/actions/actionCreators.js
+++ src/scripts/actions/actionCreators.js
@@ -1,7 +1,23 @@
 import {actionTypes} from './actionTypes';
 
-export function generateQuestions(questions){
+export function logIn(isAuthorized, loggedInAs){
     return {
+        type: actionTypes.LOG_IN,
+        payload: isAuthorized,
+        data: loggedInAs
+    };
+}
 
+export function toggleRegistered(isRegistered){
+    return {
+        type: actionTypes.TOGGLE_REGISTERED,
+        payload: isRegistered,
+    };
+}
+
+export function updateCurrentPage(currentPage){
+    return {
+        type: actionTypes.UPDATE_CURRENT_PAGE,
+        payload: currentPage,
     };
 }
