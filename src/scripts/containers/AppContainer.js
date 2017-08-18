640f76ecd5c3f484eb8e39b4244fefc906688675
Unknown <lizaburaya@gmail.com>
2017-08-18 17:47:46 +0300

    Added state and dispatch to AppContainer

--- src/scripts/containers/AppContainer.js
+++ src/scripts/containers/AppContainer.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { generateQuestions} from '../actions/actionCreators';
+import { logIn, toggleRegistered, updateCurrentPage } from '../actions/actionCreators';
 import RenderApp from '../components/RenderApp.jsx';
 
 const mapStateToProps = (state) => {
     return {
-
+        isAuthorized: state.renderApp.isAuthorized,
+        loggedInAs: state.renderApp.loggedInAs,
+        isRegistered: state.renderApp.isRegistered,
+        currentPage: state.renderApp.currentPage
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        handleLogInOutClick:
+            bindActionCreators(logIn, dispatch),
+        handleRegisteredClick:
+            bindActionCreators(toggleRegistered, dispatch),
+        handleUpdatePage:
+            bindActionCreators(updateCurrentPage, dispatch)
     }
 };
 
