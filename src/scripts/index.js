480cf17c829072ac1b5dfcdb6cd6c33380e0aeba
Unknown <lizaburaya@gmail.com>
2017-08-21 19:40:58 +0300

    Added middleware

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './custom_scripts/forms-script.js';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import AppContainer from './containers/AppContainer';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f)
+);
 
 ReactDOM.render((
     <Provider store={store}>
