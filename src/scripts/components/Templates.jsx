cf8a7f16452407f158b67a895ea5eee93cbadff4
Unknown <lizaburaya@gmail.com>
2017-08-22 18:40:54 +0300

    Moved onLoad to redux action, added spinner on isFetching

--- src/scripts/components/Templates.jsx
+++ src/scripts/components/Templates.jsx
@@ -1,42 +1,26 @@
 import React from 'react';
 import Sidebar from './Sidebar.jsx';
 import RenderTemplate from './RenderTemplate.jsx';
+import Spinner from 'react-spinner-material';
 import { Link } from 'react-router-dom';
 
 export default class Templates extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [{'name':'Нет данных','pages':'нет данных','questions':'нет данных', 'description':'нет данных'}],
             filterText: '',
         };
         this.handleChange = this.handleChange.bind(this);
-        this.onLoad = this.onLoad.bind(this);
         this.handleDeleteTemplate = this.handleDeleteTemplate.bind(this);
         this.handleEditTemplate = this.handleEditTemplate.bind(this);
     }
 
-    onLoad() {
-        $.ajax({
-            url: 'http://localhost:3000/surveys',
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-            success: function(data) {
-                let templates = data.filter((obj) => obj.template === true);
-                this.setState({data: templates});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    }
-
     componentDidMount() {
-        this.onLoad();
         if (this.props.currentPage !== '/templates')
         {
             this.props.handleChangePage('/templates');
         }
+        this.props.handleLoadSurveyData();
     }
 
     handleChange(event) {
@@ -56,7 +40,7 @@ export default class Templates extends React.Component {
                     'template':false
                 }),
                 success: function() {
-                    this.onLoad();
+                    this.props.handleLoadSurveyData();
                 }.bind(this),
                 error: function(xhr, status, err) {
                     console.error(this.props.url, status, err.toString());
@@ -65,7 +49,7 @@ export default class Templates extends React.Component {
         }
     }
 
-    handleEditTemplate(event, target, display) {
+    handleEditTemplate(target, display) {
         function show() {
             $(target).parent('.template-wrapper').find('.active-template').show();
             $('.template').css('box-shadow', 'none');
@@ -86,6 +70,8 @@ export default class Templates extends React.Component {
     }
 
     render() {
+        const { surveyData, isFetching } = this.props;
+
         return (
             <main className='d-flex flex-row justify-content-start'>
                 <Sidebar
@@ -95,15 +81,26 @@ export default class Templates extends React.Component {
                     <div className='page-head d-flex justify-content-between align-items-center'>
                         <h1>Шаблоны <Link to='/new_survey/' className='create-survey'>Новый шаблон</Link></h1>
                         <div className='search-form'>
-                            <input className='form-control' type='text' placeholder='Поиск' value={this.state.filterText} onChange={() => this.handleChange(this)} />
+                            <input className='form-control' type='text' placeholder='Поиск' value={this.state.filterText} onChange={(e) => this.handleChange(e)} />
                         </div>
                     </div>
-                    <RenderTemplate
-                        handleDeleteTemplate={this.handleDeleteTemplate}
-                        handleEditTemplate={this.handleEditTemplate}
-                        filterText={this.state.filterText}
-                        data={this.state.data}
-                    />
+                    {isFetching &&
+                        <div className='spinner'>
+                            <Spinner
+                                size={120}
+                                spinnerColor={"#FF0000"}
+                                spinnerWidth={2}
+                                visible={true} />
+                        </div>
+                    }
+                    {!isFetching &&
+                        <RenderTemplate
+                            handleDeleteTemplate={this.handleDeleteTemplate}
+                            handleEditTemplate={this.handleEditTemplate}
+                            filterText={this.state.filterText}
+                            data={surveyData}
+                        />
+                    }
                 </div>
             </main>
         )
