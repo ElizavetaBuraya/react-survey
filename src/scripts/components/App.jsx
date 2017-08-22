9f1756f7148677af8eb853e9df065671fe0b87ac
Unknown <lizaburaya@gmail.com>
2017-08-22 18:41:58 +0300

    Added space

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -18,7 +18,7 @@ export default class App extends React.Component {
             handleCreateUserClick,
             handleUpdatePage,
             handleLoadUserData,
-            handleLoadSurveyData} = this.props;
+            handleLoadSurveyData } = this.props;
 
         return (
             <BrowserRouter basename={'/'}>
