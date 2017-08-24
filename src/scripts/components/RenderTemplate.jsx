e9bb781a1f7d99438041fb9d8731355abc113dc2
Unknown <lizaburaya@gmail.com>
2017-08-24 17:20:36 +0300

    Changed template render
    Now data, if filtered, is set before template render

--- src/scripts/components/RenderTemplate.jsx
+++ src/scripts/components/RenderTemplate.jsx
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default class RenderTemplate extends React.Component {
     render() {
-        let templates = this.props.data.map(function(obj, index) {
         let { filterText, handleEditTemplate, handleDeleteTemplate, data } = this.props;
+        let templateData = (filterText)
+            ? data.filter((obj) =>
+                obj.name.includes(filterText)
+                    || obj.description.includes(filterText)
+                    || obj.pages.toString().includes(filterText)
+                    || obj.questions.toString().includes(filterText))
+                : data;
+
+
+        let templates = templateData.map(function(obj, index) {
             return <div className='template-wrapper' key={index}>
                         <p className='active-template'>
                             <Link to={`/new_survey/${obj.link}`} className='edit-template'/>
@@ -23,36 +32,10 @@ export default class RenderTemplate extends React.Component {
                         </div>
                    </div>
         });
-        let filteredTemplates = this.props.data.map(function(obj, index) {
-            if ((that.props.filterText) &&
-                obj.name.includes(that.props.filterText) ||
-                obj.description.includes(that.props.filterText) ||
-                obj.pages.toString().includes(that.props.filterText) ||
-                obj.questions.toString().includes(that.props.filterText)
-            ) {
-                return <div className='template-wrapper' key={index}>
-                            <p className='active-template'>
-                                <Link to={`/new_survey/${obj.link}`} className='edit-template'/>
-                                <span className='delete-template' onClick={() => that.props.handleDeleteTemplate(obj.id)}/>
-                            </p>
-                            <div className='template'
-                                 tabIndex='0'
-                                 onClick={(e) => that.props.handleEditTemplate(e.currentTarget)}
-                                 onBlur={(e) => that.props.handleEditTemplate(e.currentTarget, 'hide')}
-                            >
-                                <h2>{ obj.name }</h2>
-                                <p>{ obj.description }</p>
-                                <p className='question-number'>Вопросов: <span>{ obj.questions }&nbsp;</span></p>
-                                <p className='page-number'>Страниц: <span>{ obj.pages }</span></p>
-                                <Link to={`/new_survey/create/${obj.link}`} className='create-survey'>Создать опрос</Link>
-                            </div>
-                        </div>
-            }
-        });
+
         return (
             <div className='available-templates'>
-                {!this.props.filterText && templates}
-                {this.props.filterText && filteredTemplates}
+                {templates}
             </div>
         )
     }
