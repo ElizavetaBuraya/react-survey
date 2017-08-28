9c2938611f586173a9b3a49675735e2f9a59acda
Unknown <lizaburaya@gmail.com>
2017-08-28 12:23:31 +0300

    Made Footer.jsx stateless

--- src/scripts/components/Footer.jsx
+++ src/scripts/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default class Footer extends React.Component {
-    render() {
-        return (
-            <footer className='d-flex flex-row justify-content-center'>
-                <span className='copyright'>© 2002 - 2017. :iTechArt All Rights Reserved.</span>
-            </footer>
-        )
-    }
-}
+const Footer = (props) => (
+    <footer className='d-flex flex-row justify-content-center'>
+        <span className='copyright'>© 2002 - 2017. :iTechArt All Rights Reserved.</span>
+    </footer>
+);
+
+export default Footer;
