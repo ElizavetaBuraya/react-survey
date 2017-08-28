509ed5ea04b7ae1f7aadfe1257793003259c84d3
Unknown <lizaburaya@gmail.com>
2017-08-28 12:25:40 +0300

    Made Main.jsx stateless

--- src/scripts/components/Main.jsx
+++ src/scripts/components/Main.jsx
@@ -10,154 +10,168 @@ import GenerateSurvey from './RenderSurvey.jsx';
 import SurveyResults from './SurveyResults.jsx'
 import NoMatch from './ErrorPage.jsx'
 
-export default class Main extends React.Component {
-    render() {
-        const { isFetching,
-            isAuthorized,
-            isRegistered,
-            loggedInAs,
-            userData,
-            surveyData,
-            currentPage,
-            handleRegisteredClick,
-            handleLogInClick,
-            handleChangePage,
-            handleCreateUserClick,
-            handleLoadUserData,
-            handleLoadSurveyData,
-            handleDeleteUserData,
-            handleEditUserData,
-            handleDeleteSurveyData,
-            handleEditSurveyData,
-            handleDeleteTemplate } = this.props;
+const Main = (props) => {
+    const { isFetching,
+        isAuthorized,
+        isRegistered,
+        loggedInAs,
+        userData,
+        surveyData,
+        currentPage,
+        redirect,
+        redirectPath,
+        handleRegisteredClick,
+        handleLogInClick,
+        handleChangePage,
+        handleCreateUserClick,
+        handleLoadUserData,
+        handleLoadSurveyData,
+        handleDeleteUserData,
+        handleEditUserData,
+        handleDeleteSurveyData,
+        handleEditSurveyData,
+        handleDeleteTemplate,
+        handleUpdateSurvey,
+        handleCreateSurvey, handleLoadUser } = props;
 
-        return (
-            <Switch>
-                <Route exact path='/'
-                       component={() => <LoginRegister
-                           isRegistered = {isRegistered}
-                           isAuthorized = {isAuthorized}
-                           handleRegisteredClick = {handleRegisteredClick}
-                           handleCreateUserClick = {handleCreateUserClick}
-                           handleLogInClick = {handleLogInClick}
-                           handleChangePage = {handleChangePage}
-                           currentPage = {currentPage}
-                       />}
-                />
-                <Route path='/about' render={() => (
-                    !isAuthorized ? (
-                        <Redirect to='/'/>
-                    ) : (
-                        <About
-                            isAuthorized={isAuthorized}
-                            handleChangePage = {handleChangePage}
-                            currentPage = {currentPage}
-                            loggedInAs = {loggedInAs}
-                        />
-                    )
-                )}/>
-                <Route path='/users' render={() => (
-                        !isAuthorized || (loggedInAs.role === 'Пользователь') ? (
-                            <Redirect to='/'/>
-                        ) : (
-                            <Users
-                                handleChangePage = {handleChangePage}
-                                handleLoadUserData = {handleLoadUserData}
-                                handleDeleteUserData = {handleDeleteUserData}
-                                handleEditUserData = {handleEditUserData}
-                                currentPage = {currentPage}
-                                loggedInAs = {loggedInAs}
-                                userData = {userData}
-                                isFetching = {isFetching}
-                            />
-                        )
-                )}/>
-                <Route path='/surveys' render={() => (
-                    !isAuthorized ? (
-                        <Redirect to='/'/>
-                    ) : (
-                        <Surveys
-                            handleChangePage = {handleChangePage}
-                            handleLoadSurveyData = {handleLoadSurveyData}
-                            handleDeleteSurveyData = {handleDeleteSurveyData}
-                            handleEditSurveyData = {handleEditSurveyData}
-                            currentPage = {currentPage}
-                            loggedInAs = {loggedInAs}
-                            surveyData = {surveyData}
-                            isFetching = {isFetching}
-                        />
-                    )
-                )}/>
-                <Route path='/templates' render={() => (
-                    !isAuthorized  || (loggedInAs.role === 'Пользователь') ? (
-                        <Redirect to='/'/>
-                    ) : (
-                        <Templates
-                            handleChangePage = {handleChangePage}
-                            handleLoadSurveyData = {handleLoadSurveyData}
-                            handleDeleteTemplate = {handleDeleteTemplate}
-                            currentPage = {currentPage}
-                            surveyData = {surveyData}
-                            isFetching = {isFetching}
-                        />
-                    )
-                )}/>
-                <Route exact path='/new_survey' render={() => (
-                    !isAuthorized  || (loggedInAs.role === 'Пользователь') ? (
-                        <Redirect to='/'/>
-                    ) : (
-                        <NewSurvey
-                            handleChangePage = {handleChangePage}
-                            currentPage = {currentPage}
-                        />
-                    )
-                )}/>
-                <Route path='/new_survey/:link'
-                       render={(props) => (
-                           !isAuthorized ? (
-                               <Redirect to='/'/>
-                           ) : (
-                               <NewSurvey {...props}
-                                          currentPage='new_survey/:link'
-                               />
-                           )
-                       )}
-                />
-                <Route path='/survey/:link/results'
-                       render={(props) => (
-                           !isAuthorized ? (
-                               <Redirect to='/'/>
-                           ) : (
-                               <SurveyResults {...props}
-                                              currentPage='survey/:link/results'
-                               />
-                           )
-                       )}
-                />
-                <Route path='/survey/:link'
-                       render={(props) => (
-                           !isAuthorized ? (
-                               <Redirect to='/'/>
-                           ) : (
-                               <GenerateSurvey {...props}
-                                               loggedInAs = {loggedInAs}
-                               />
-                           )
-                       )}
-                />
-                <Route render={() => (
-                    !isAuthorized ? (
-                        <Redirect to='/'/>
-                    ) : (
-                        <NoMatch
-                            isRegistered={isRegistered}
-                            isAuthorized={isAuthorized}
-                            handleChangePage = {handleChangePage}
-                            currentPage = {currentPage}
-                        />
-                    )
-                )}/>
-            </Switch>
-        )
-    }
-}
+    return (
+        <Switch>
+            <Route exact path='/'
+                   component={() => <LoginRegister
+                       isRegistered = {isRegistered}
+                       isAuthorized = {isAuthorized}
+                       handleRegisteredClick = {handleRegisteredClick}
+                       handleCreateUserClick = {handleCreateUserClick}
+                       handleLogInClick = {handleLogInClick}
+                       handleChangePage = {handleChangePage}
+                       currentPage = {currentPage}
+                   />}
+            />
+            <Route path='/about' render={() => (
+                !isAuthorized ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <About
+                        isAuthorized={isAuthorized}
+                        handleChangePage = {handleChangePage}
+                        currentPage = {currentPage}
+                        loggedInAs = {loggedInAs}
+                    />
+                )
+            )}/>
+            <Route path='/users' render={() => (
+                !isAuthorized || (loggedInAs.role === 'Пользователь') ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <Users
+                        handleChangePage = {handleChangePage}
+                        handleLoadUserData = {handleLoadUserData}
+                        handleDeleteUserData = {handleDeleteUserData}
+                        handleEditUserData = {handleEditUserData}
+                        currentPage = {currentPage}
+                        loggedInAs = {loggedInAs}
+                        userData = {userData}
+                        isFetching = {isFetching}
+                    />
+                )
+            )}/>
+            <Route path='/surveys' render={() => (
+                !isAuthorized ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <Surveys
+                        handleChangePage = {handleChangePage}
+                        handleLoadSurveyData = {handleLoadSurveyData}
+                        handleDeleteSurveyData = {handleDeleteSurveyData}
+                        handleEditSurveyData = {handleEditSurveyData}
+                        currentPage = {currentPage}
+                        loggedInAs = {loggedInAs}
+                        surveyData = {surveyData}
+                        isFetching = {isFetching}
+                       />
+                )
+            )}/>
+            <Route path='/templates' render={() => (
+                !isAuthorized  || (loggedInAs.role === 'Пользователь') ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <Templates
+                        handleChangePage = {handleChangePage}
+                        handleLoadSurveyData = {handleLoadSurveyData}
+                        handleDeleteTemplate = {handleDeleteTemplate}
+                        currentPage = {currentPage}
+                        surveyData = {surveyData}
+                        isFetching = {isFetching}
+                    />
+                )
+            )}/>
+            <Route exact path='/new_survey' render={() => (
+                !isAuthorized  || (loggedInAs.role === 'Пользователь') ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <NewSurvey
+                        handleChangePage = {handleChangePage}
+                        handleCreateSurvey = {handleCreateSurvey}
+                        redirect = {redirect}
+                        redirectPath = {redirectPath}
+                        currentPage = {currentPage}
+                    />
+                )
+            )}/>
+            <Route path='/new_survey/:link'
+                   render={(props) => (
+                       !isAuthorized ? (
+                           <Redirect to='/'/>
+                       ) : (
+                           <NewSurvey {...props}
+                                      handleCreateSurvey = {handleCreateSurvey}
+                                      handleUpdateSurvey = {handleUpdateSurvey}
+                                      redirect = {redirect}
+                                      redirectPath = {redirectPath}
+                                      currentPage='new_survey/:link'
+                           />
+                       )
+                   )}
+            />
+            <Route path='/survey/:link/results'
+                   render={(props) => (
+                       !isAuthorized ? (
+                           <Redirect to='/'/>
+                       ) : (
+                           <SurveyResults {...props}
+                                          currentPage='survey/:link/results'
+                           />
+                       )
+                   )}
+            />
+            <Route path='/survey/:link'
+                   render={(props) => (
+                       !isAuthorized ? (
+                           <Redirect to='/'/>
+                       ) : (
+                           <GenerateSurvey {...props}
+                                           handleLoadUser = {handleLoadUser}
+                                           redirect = {redirect}
+                                           redirectPath = {redirectPath}
+                                           loggedInAs = {loggedInAs}
+                           />
+                       )
+                   )}
+            />
+            <Route render={() => (
+                !isAuthorized ? (
+                    <Redirect to='/'/>
+                ) : (
+                    <NoMatch
+                        isRegistered={isRegistered}
+                        isAuthorized={isAuthorized}
+                        handleChangePage = {handleChangePage}
+                        currentPage = {currentPage}
+                    />
+                )
+            )}/>
+        </Switch>
+    )
+};
+
+export default Main;
