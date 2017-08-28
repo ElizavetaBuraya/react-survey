55ff7c7c4508196f8248a31bdffaa3484b306170
Unknown <lizaburaya@gmail.com>
2017-08-28 12:24:03 +0300

    Made GenerateResults.jsx stateless

--- src/scripts/components/GenerateResults.jsx
+++ src/scripts/components/GenerateResults.jsx
@@ -2,62 +2,61 @@ import React from 'react';
 import Answer from './Answer.jsx';
 import AnswerStats from './AnswerStats.jsx'
 
-export default class GenerateResults extends React.Component {
-    render() {
-        const { handleToggleCharts,
-            questions_list,
-            survey_page,
-            currentPage,
-            questions_are_numbered,
-            required_fields,
-            displayChart,
-            user_results } = this.props;
+const GenerateResults = (props) => {
+    const { handleToggleCharts,
+        questions_list,
+        survey_page,
+        currentPage,
+        questions_are_numbered,
+        required_fields,
+        displayChart,
+        user_results } = props;
 
-        if (!displayChart) {
-            return (
-                <div>
-                    {questions_list[survey_page].map((question, index) =>
-                        <Answer
-                            key={question.id}
-                            index={index}
-                            title={question.title}
-                            type={question.type}
-                            required={question.required}
-                            answers={question.answers}
-                            result={question.result}
-                            questions_list = {questions_list}
-                            survey_page = {survey_page}
-                            currentPage = {currentPage}
-                            questions_are_numbered = {questions_are_numbered}
-                            required_fields = {required_fields}
-                        />
-                    )}
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    {questions_list[survey_page].map((question, index) =>
-                        <AnswerStats
-                            key={question.id}
-                            id={question.id}
-                            index={index}
-                            title={question.title}
-                            type={question.type}
-                            user_results={user_results}
-                            required={question.required}
-                            answers={question.answers}
-                            result={question.result}
-                            questions_list = {questions_list}
-                            survey_page = {survey_page}
-                            currentPage = {currentPage}
-                            questions_are_numbered = {questions_are_numbered}
-                            required_fields = {required_fields}
-                            handleToggleCharts = {handleToggleCharts}
-                        />
-                    )}
-                </div>
-            )
-        }
+    if (!displayChart) {
+        return (
+            <div>
+                {questions_list[survey_page].map((question, index) => <Answer
+                        key={question.id}
+                        index={index}
+                        title={question.title}
+                        type={question.type}
+                        required={question.required}
+                        answers={question.answers}
+                        result={question.result}
+                        questions_list = {questions_list}
+                        survey_page = {survey_page}
+                        currentPage = {currentPage}
+                        questions_are_numbered = {questions_are_numbered}
+                        required_fields = {required_fields}
+                    />
+                )}
+            </div>
+        )
+    } else {
+        return (
+               <div>
+                   {questions_list[survey_page].map((question, index) =>
+                       <AnswerStats
+                           key={question.id}
+                           id={question.id}
+                           index={index}
+                           title={question.title}
+                           type={question.type}
+                           user_results={user_results}
+                           required={question.required}
+                           answers={question.answers}
+                           result={question.result}
+                           questions_list = {questions_list}
+                           survey_page = {survey_page}
+                           currentPage = {currentPage}
+                           questions_are_numbered = {questions_are_numbered}
+                           required_fields = {required_fields}
+                           handleToggleCharts = {handleToggleCharts}
+                       />
+                   )}
+               </div>
+        )
     }
-}
+};
+
+export default GenerateResults;
