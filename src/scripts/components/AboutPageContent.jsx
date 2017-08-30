1026ae010faab15f5f15b3605b0ad8a8b241899c
Unknown <lizaburaya@gmail.com>
2017-08-30 11:31:12 +0300

    Created AboutPageContent component

---
+++ src/scripts/components/AboutPageContent.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+const AboutPageContent = (props) => {
+    const content = <div>
+        {props.index === 0 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 1 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 2 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 3 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 4 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 5 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 6. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+        {props.index === 6 && <div>
+            <img src='img/placeholder_2.png' alt='placeholder' height='200' />
+            <p>
+                Page 7. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
+                elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
+                Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
+                fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
+                eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus.
+            </p>
+        </div>}
+    </div>;
+
+    return(content)
+};
+
+export default AboutPageContent;
