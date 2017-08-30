db6dfd4f05f4e1029576bc4ccc4244de0d03d833
Unknown <lizaburaya@gmail.com>
2017-08-30 11:30:36 +0300

    Imported AboutPageContent

--- src/scripts/components/Tabs.jsx
+++ src/scripts/components/Tabs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import GenerateQuestions from './GenerateQuestions.jsx'
+import AboutPageContent from './AboutPageContent.jsx'
 import GenerateResults from './GenerateResults.jsx'
 
 function TabItem(props) {
@@ -109,18 +110,7 @@ const GenerateContent = (props) => {
                     </div>
                     }
                     {isAboutPage &&
-                    <div>
-                        <img src='img/placeholder_2.png' alt='placeholder' height='200' />
-                        <p>
-                            Page {index + 1}.<br />
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper
-                            elementum libero sed lacinia. Quisque ac quam tempus, aliquet nibh vitae, lobortis ex.
-                            Aliquam posuere interdum ex vitae pretium. Mauris ac odio in ex feugiat varius. Nam nec
-                            fringilla diam. Phasellus ac nibh sit amet nisl viverra posuere. Nulla vehicula commodo eros
-                            eu posuere. Fusce finibus ligula pharetra, fringilla elit et, vestibulum mauris. Interdum et
-                            malesuada fames ac ante ipsum primis in faucibus.
-                        </p>
-                    </div>
+                        <AboutPageContent index={index}/>
                     }
                 </div>
             )}
